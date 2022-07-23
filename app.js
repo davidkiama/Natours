653d12eac0fc216fc04c60f2ef98552f975da188
davidkiama <davidkiama98@gmail.com>
2022-07-23 13:19:34 +0300

    Setting up the API to handle GET requests

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello for the server", app: "app.js" });
-});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+console.log(tours);
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const PORT = 3000;
