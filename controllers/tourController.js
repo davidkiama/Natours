9ab43bc782d47906a1c2907e3d6c7ac272a3190b
davidkiama <davidkiama98@gmail.com>
2022-07-25 13:28:40 +0300

    Chaining multiple middlewares

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,19 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  const { name, price } = req.body;
+
+  if (!name || !price) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Missing name or price",
+    });
+  }
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
